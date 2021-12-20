2288f4c11669a42fc77c71411150cb4b70412c1f
nmgomezok <nmgomezok@gmail.com>
2021-12-20 00:30:12 -0300

    correccion de error que al subir producto no se renderiza imagen

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -9,21 +9,19 @@ const { body } = require('express-validator');
 
 
 const storage = multer.diskStorage({
-
-    destination: (req, file, cb) => {
-    if(req.body.description){
-        cb(null, './public/img/products')
-    }else{
-        cb(null, './public/img/profile')
-    }
-    },
+	destination: (req, file, cb) => {
+		if(req.body.description){
+			cb(null, './public/img')
+		}else{
+			cb(null, './public/img/profile')
+		}
+		},
     filename: (req, file, cb) => {
         let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
-        cb(null, fileName)
+        cb(null,fileName)
     }
 })
-
-const uploadFile= multer({ storage });
+const upload = multer({ storage: storage })
 
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
@@ -34,7 +32,7 @@ const validationsCreateProduct = [
     body('description').notEmpty().withMessage('Tienes que escribir una descripcion'),
     body('producto').custom((value, { req }) => {
 		let file = req.file;
-		let acceptedExtensions = ['.jpg', '.png', '.gif'];
+		let acceptedExtensions = ['.jpg', '.png', '.gif', '.png'];
 		
 		if (!file) {
 			throw new Error('Tienes que subir una imagen');
@@ -81,15 +79,15 @@ router.post('/login', maincontroller.loginProcess);
 router.get('/productCart', maincontroller.productCart);
 
 router.get('/register', guestMiddleware, maincontroller.register);
-router.post('/register', uploadFile.single('avatar'), uservalidation, maincontroller.registerCreate);
+router.post('/register', upload.single('avatar'), uservalidation, maincontroller.registerCreate);
 
 router.get('/productList', producListController.productList);
 
 router.get('/createProduct', producListController.createProduct);
-router.post('/createProduct', uploadFile.single('producto'), validationsCreateProduct, producListController.abmproduct);
+router.post('/createProduct', upload.single("producto"), validationsCreateProduct, producListController.abmproduct);
 
 router.get('/editProduct/:id', producListController.getProductToEdit);
-router.put('/:id', uploadFile.single('producto'), producListController.editProduct);
+router.put('/:id', upload.single('producto'), producListController.editProduct);
 
 router.get('/productList', producListController.productList);
 router.delete('/:id', producListController.deleteProduct);
