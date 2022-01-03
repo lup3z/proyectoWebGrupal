8a073441cf336c712ef88cf9ec3991f4cae6e241
Guillermo Barra <guillermosebastianbarra@.gmail.com>
2022-01-03 15:56:54 -0300

    pequenio  cammbios apis

--- src/controllers/apiProductsController.js
+++ src/controllers/apiProductsController.js
@@ -3,20 +3,20 @@ const apiProductsModel = require('../models/apiProductsModel');
 const controller = {
 
     allProducts: async function (req, res) {
-        try {
-            // const categoryJinete = await apiProductsModel.findAll({where: {idProductsCategory: {[op.eq]: 1}}}).then((resultado => {return resultado}))
+        try { 
+           
             const products = await apiProductsModel.findAll();
             res.status(200).json({
                 count: products.length,
-                // countByCategory: {jinete: categoryJinete.length},
+               
                 products: products.map(item => ({   
                     data: item,
-                    detail: `http://localhost:3000/api/products/${item.id}`
+                    detail: `http://localhost:3030/api/products/${item.id}`
                 })), 
                 status: 200
                 })
         } catch (error) {
-            res.status(404).render('404-page.ejs');
+            res.status(404).render('not-found');
         }
     },
     
@@ -28,7 +28,7 @@ const controller = {
                     status: 200
                 });
         } catch (error) {
-            res.status(404).render('404-page.ejs');
+            res.status(404).render('not-found');
         }
     }
 }
